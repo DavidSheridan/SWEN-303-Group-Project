37ec12a310f7d7e68188379ca1630947abc27403
Shane Brewer <theproros@gmail.com>
2015-06-22 15:03:52 +1200

    Made the filter need the graphs made so that the calls can be added to modify them

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,71 +1,58 @@
 function make_season_filter(){
-	console.log('Made indicators');
-	var filter = d3.select('#filters');
+	var margin = {top: 0, right: 40, bottom: 30, left: 10},
+	    width = 900 - margin.left - margin.right,
+	    height = 80 - margin.top - margin.bottom;
+	
+	var x = d3.scale.quantile()
+		.range([0, width - (width/5)*4, width - (width/5)*3, 
+	    	width - (width/5)*2, width - (width/5)*1, 
+	    	width - (width/5)*0])
+	    .domain([2008, 2009, 2010, 2011, 2012, 2013]);
 
-	var carousel = filter.append('div')
-		.attr('id', 'season-filter')
-		.attr('data-ride', 'carousel')
-		.style('height', 80)
-		.classed('carousel slide', true);
+	var brush = d3.svg.brush()
+	    .x(x)
+	    .extent([0, width/5-30])
+	    .on("brushstart", brushstart)
+	    .on("brush", brushmove)
+	    .on("brushend", brushend);
 
-	var indicators = carousel.append('ol')
-		.classed('carousel-indicators',true);
+	var svg = d3.select("#filters").append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	  	.append("g")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	
+	svg.append("g")
+	    .attr("class", "x axis")
+	    .attr("transform", "translate(0," + height + ")")
+	    .call(d3.svg.axis().scale(x).orient("bottom"));
 
-	var wrapers = carousel.append('div')
-		.classed('carousel-inner',true)
-		.attr('role', 'listbox');
+	
+	var brushg = svg.append("g")
+	    .attr("class", "brush")
+	    .call(brush);
+	
+	brushg.selectAll(".resize").append("path")
+	    .attr("transform", "translate(0," +  height / 2 + ")")
+	
+	brushg.selectAll("rect")
+	    .attr("height", height);
 
-	var season = wrapers.append('div')
-		.classed('item active', true);
+	
+	brushstart();
+	brushmove();
+	
+	function brushstart() {
+	  svg.classed("selecting", true);
+	}
+	
+	function brushmove() {
+	  var s = brush.extent();
 
-	season.append('img')
-		.attr('src', 'http://www.anz-championship.com/portals/6/Images/Team%20Logos/200x150/thunderbirds.png')
-		.style('width', 'auto')
-		.style('height', '70')
-	season.append('div')
-		.classed('carousel-caption', true)
-		.text('Test two');
+	}
+	
+	function brushend() {
+	  svg.classed("selecting", !d3.event.target.empty());
+	}
 
-	season = wrapers.append('div')
-		.classed('item', true);
-
-	season.append('img')
-		.attr('src', 'http://www.anz-championship.com/portals/6/Images/Team%20Logos/200x150/pulse.png')
-		.style('width', 'auto')
-		.style('height', '70')
-	season.append('div')
-		.classed('carousel-caption', true)
-		.text('Test One');
-
-
-	var left_control = carousel.append('a')
-		.classed('left carousel-control', true)
-		.attr('href', '#season-filter')
-		.attr('role', 'button')
-		.attr('data-slide', 'prev')
-		.on('click', function(){
-			console.log('Prevous selection');
-		});
-	left_control.append('span')
-		.classed('glyphicon glyphicon-chevron-left',true)
-		.attr('aria-hidden', 'true');
-	left_control.append('span')
-		.classed('sr-only',true)
-		.text('Previous');
-
-	var right_control = carousel.append('a')
-		.classed('right carousel-control', true)
-		.attr('href', '#season-filter')
-		.attr('role', 'button')
-		.attr('data-slide', 'next')
-		.on('click', function(){
-			console.log('Next selection');
-		});
-
-	right_control.append('span')
-		.classed('glyphicon glyphicon-chevron-right',true)
-		.attr('aria-hidden', 'true');
-	right_control.append('span')
-		.classed('sr-only',true)
-		.text('Next');
 }
